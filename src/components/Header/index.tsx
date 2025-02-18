c935d473d6d2550315e13fa74c44d3e36128ccff
gabrieltsujimoto <gabrieltsujimotovicente@outlook.com>
2025-02-18 13:36:01 -0300

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,16 @@
+import { Button, Flex, Spacer } from '@chakra-ui/react'
 import './index.css'
 
 export const Header = () => {
     return (
-        <div className='header'>
-            <h1 className='header-title'>💰 DIO Bank 💰</h1>
-            <p className='header-subtitle'>Faça seu login agora!</p>
-        </div>
+        <Flex border={'1px solid white'} alignItems={'center'}>
+            <div className='header'>
+                <h1 className='header-title'>💰 DIO Bank 💰</h1>
+                <p className='header-subtitle'>Faça seu login agora!</p>
+            </div>
+            <Spacer position={'relative'} right={0} border={'1px solid white'} display={'flex'} alignItems={'center'} paddingX={2}>
+                <Button>Sair</Button>
+            </Spacer>
+        </Flex>
     )
 }
