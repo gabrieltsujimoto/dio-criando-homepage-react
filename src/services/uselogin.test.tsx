fbd4811385e1baff37253abdd2a4f10705f42374
gabrieltsujimoto <gabrieltsujimotovicente@outlook.com>
2025-02-20 18:30:38 -0300

    update package.json

--- src/services/uselogin.test.tsx
+++ src/services/uselogin.test.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import { UseLogin } from "./UseLogin";
+import { AppContext } from "../components/context/AppContext";
 /*jest.mock('react-router', () => {
      const actual = jest.requireActual('react-router-dom');
      return {
@@ -12,19 +14,20 @@ describe('login', () => {
     const mockMail = 'gabriel@teste.com';
     const mockPass = '123456'
     window.alert = mockAlert;
+    const isLoggedIn = true;
 
     it('Deve retornar falso caso login ou senha sejam inválidos com os da API', async () => {
-        const res = await UseLogin('email@invalido', 'senhainvalida');
+        const res = await UseLogin('email@invalido', 'senhainvalida', isLoggedIn);
         expect(res).toBeFalsy();
     })
 
     it('Deve retornar verdadeiro caso e-mail seja válido', async () => {
-        const res = await UseLogin(mockMail, mockPass);
+        const res = await UseLogin(mockMail, mockPass, isLoggedIn);
         expect(res).toBeTruthy();
     })
 
     it('Deve exibir um erro caso o e-mail seja invalido.', async () => {
-        const res = await UseLogin('email@invalido.erro', '12345');
+        const res = await UseLogin('email@invalido.erro', '12345', isLoggedIn);
         expect(res).toBeFalsy();
 
         /*  expect(mockSetIsLoggedIn).not.toHaveBeenCalled()
